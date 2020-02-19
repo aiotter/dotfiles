df04eacdc022217094bc14eeb3a85cb30d1d429b
aiotter <ogran.std@gmail.com>
2020-02-19 14:35:40 +0900

    tmux: Use nerdBattery instead of Battery

--- tmux/responsive-window-status-r.sh
+++ tmux/responsive-window-status-r.sh
@@ -5,6 +5,8 @@ WIDTH=${1}
 SMALL=80
 MEDIUM=140
 
+# battery にパスを通す
+PATH=$HOME/local/bin:$PATH
 
 # オンラインステータスの取得
 ping 'google.com' -c 1 >/dev/null 2>&1
@@ -23,11 +25,11 @@ fi
 if [ "$WIDTH" -gt "$MEDIUM" ]
 then
     # 画面幅が十分大きいとき
-    echo " #[fg=cyan]${online_status} $(battery -t -g blue) DOCK:$containers $(date +'[%a %d-%m-%Y %H:%M]')"
+    echo " #[fg=cyan]${online_status} $(battery -t -g blue)  DOCK:$containers $(date +'[%a %d-%m-%Y %H:%M]') "
 elif [ "$WIDTH" -ge "$SMALL" ]
 then
     # 画面幅が中くらいのとき
-    echo " #[fg=cyan]$(battery -t -g blue) $(date +'[%a %d-%m-%Y %H:%M]')"
+    echo " #[fg=cyan]$(battery -t -g blue)  $(date +'[%a %d-%m-%Y %H:%M]')"
 else
     # 画面幅が十分小さいとき
     echo ""
