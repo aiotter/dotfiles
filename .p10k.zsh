529e4d1d8341388301aae0da64066b13d555b868
aiotter <git@aiotter.com>
2020-07-26 01:08:39 +0900

    zsh: Simplified the prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -98,15 +98,15 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vi_mode               # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
-    load                    # CPU load
+    # load                    # CPU load
     # disk_usage            # disk usage
-    ram                     # free RAM
+    # ram                     # free RAM
     # swap                  # used swap
     # todo                  # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # time                  # current time
-  docker
+    # docker
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
