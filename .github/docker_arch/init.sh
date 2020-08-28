1e7e760ff4dbb7b65d5533896e575115c9bc5329
aiotter <git@aiotter.com>
2020-08-28 11:11:08 +0900

    Do not use USER instruction in Dockerfile
    As adviced [here](https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions#user).
    Now 'input' is not necessary because the environment variables are
    passed automatically to user 'dot' via `sudo --preserve-env --user=dot`.

--- .github/docker_arch/init.sh
+++ .github/docker_arch/init.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -l
 
-export CI='true'
-export GITHUB_REPOSITORY=$1
-export GITHUB_SHA=$2
-
 sudo pacman -S --noconfirm --needed git
-git clone "https://github.com/${GITHUB_REPOSITORY} dotfiles"
+git clone "https://github.com/${GITHUB_REPOSITORY}" dotfiles
 git checkout "$GITHUB_SHA"
 
-dotfiles/deploy/deploy.sh
+sudo --preserve-env --user=dot HOME=/home/dot dotfiles/deploy/deploy.sh
