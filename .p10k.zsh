02ba52f3c76352bd43a886ac909ea28cf57422a9
aiotter <git@aiotter.com>
2021-06-25 05:12:35 +0900

    zsh: Shorten prompt a little bit

--- .p10k.zsh
+++ .p10k.zsh
@@ -564,7 +564,7 @@
   # Custom icon.
   # typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_VISUAL_IDENTIFIER_EXPANSION='⭐'
   # Custom prefix.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PREFIX='took '
+  # typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PREFIX='took '
 
   #######################[ background_jobs: presence of background jobs ]#######################
   # Background jobs color.
